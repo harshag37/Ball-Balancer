942b67300daeb7a1f3ae6b75f10db48d9776df40
abhishek <62871212+abhishek2720@users.noreply.github.com>
2020-08-23 22:20:09 +0530

    Add files via upload

---
+++ abhishek.py
@@ -0,0 +1,21 @@
+import random
+p=int(input("enter the no -"))
+a=random.sample(range(0,p),10)
+print(a)
+l=len(a)
+i=0
+while i<10:
+    if a[i]>5:
+    	print(a[i])
+    	print(" ")
+    	i=i+1
+
+
+
+
+
+
+
+
+
+
