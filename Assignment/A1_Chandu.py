443d384d472efc87befc2bfa56752c8b5f7cec64
Chandu Garudachalam <69913855+ChanduGaruda012@users.noreply.github.com>
2020-08-28 13:16:57 +0530

    Add files via upload

---
+++ Assignment/A1_Chandu.py
@@ -0,0 +1,16 @@
+import random
+
+def find(arr1,n,m):
+    if(n<0):
+        return
+    else:
+        find(arr1,n-1,m)
+        if(arr1[n]>m):
+             print(arr1[n])
+g = int(input("Enter your number : ")) 
+arr1=random.sample(range(0,g),10)
+print(arr1)
+n=len(arr1)
+m=5;
+
+find(arr1,n-1,m)
